77e5f18862ab299a2fbcd2c155ecc6c37a42fef4
Hannes Polte <hannes.polte@gmx.de>
2021-06-21 17:18:21 +0200

    added a background - jumping still improvable

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
 const jumpy = document.getElementById('character')
+const jungle = document.getElementById('background')
 const startbtn = document.getElementById('startbtn');
 const stopbtn = document.getElementById('stopbtn');
 
@@ -15,32 +16,27 @@ let counter = 0;
 
 let character = {
     img: jumpy,
-    h: 40,
-    w: 20,
-    x: canvas.width / 2,
-    y: canvas.height - 40,
+    h: 50,
+    w: 50,
+    x: (canvas.width / 2)-25,
+    y: canvas.height - 60,
     jumpCounter: 0,
     acceleration: 0.1,
-    upSpeed: 12,
+    upSpeed: 15,
     canvasSpeed: 2.5,
     jumpButton: false,
-    downMovement:false,
+    downMovement: false,
+    fallOff: false,
     draw: function () {
         ctx.drawImage(this.img, this.x, this.y, this.w, this.h)
     },
     jump: function () {
-        this
+        this.downMovement = false;
+        this.fallOff = false;
         this.y -= this.upSpeed;
         this.jumpCounter += 1;
         this.upSpeed -= this.acceleration;
-        if (this.jumpCounter % 10 === 0) { this.acceleration += 0.05 };
-        if (this.y + this.upSpeed >= canvas.height - 40) {
-            this.y = canvas.height - 40
-            this.jumpButton = false;
-            this.jumpCounter = 0;
-            this.acceleration = 0.1;
-            this.upSpeed = 12;
-        }
+        if (this.jumpCounter % 8 === 0) { this.acceleration += 0.05 };
 
     },
     land: function () {
@@ -48,19 +44,30 @@ let character = {
             this.jumpButton = false;
             this.jumpCounter = 0;
             this.acceleration = 0.1;
-            this.downMovement=true;
-            this.upSpeed=12;
+            this.downMovement = true;
+            this.upSpeed = 10;
+        }
+
+    },
+    fallOffTheEdge: function () {
+        if (checkStages() === false) {
+            this.upSpeed = -10
+            this.y -= this.upSpeed
         }
 
     },
     moveLeft: function () {
-        this.x -= 12
+        if (this.x - 20 > -10) {
+            this.x -= 20
+        }
     },
     moveRight: function () {
-        this.x += 12
+        if (this.x + 20 < canvas.width) {
+            this.x += 20
+        }
     },
-    moveDown:function(){
-        this.y+=this.canvasSpeed;
+    moveDown: function () {
+        this.y += this.canvasSpeed;
 
     }
 
@@ -71,11 +78,27 @@ let character = {
 // the background
 
 let canvasObj = {
-    width: canvas.width,
-    height: canvas.height,
+    img: jungle,
+    x: 0,
+    y: 0,
+    speed: 2.5,
+
+    move: function () {
+        this.y += this.speed;
+        this.y %= canvas.height;
+    },
+
+    draw: function () {
+        ctx.drawImage(this.img, 0, this.y);
+        if (this.speed < 0) {
+            ctx.drawImage(this.img, 0, this.y + this.img.height);
+        } else {
+            ctx.drawImage(this.img, 0, this.y - canvas.height);
+        }
+    },
+};
 
 
-}
 
 // the stages
 
@@ -94,10 +117,10 @@ class Stage {
 
 function newStage() {
 
-    if (counter % 80 === 0) {
+    if (counter % 55 === 0) {
         counter = 0;
-        let minWidth = 55;
-        let maxWidth = 150;
+        let minWidth = 80;
+        let maxWidth = 180;
         let x = Math.floor(Math.random() * (canvas.width - minWidth));
         let y = -15;
         let width = Math.floor(Math.random() * (maxWidth - minWidth)) + minWidth;
@@ -140,16 +163,14 @@ function checkStages() {
         let stageX = stage.x;
         let stageY = stage.y;
         let stageWidth = stage.width;
-        if (jumpyY + character.upSpeed > stageY && jumpyY + character.upSpeed < stageY + 20) {
-            if (jumpyX >= stageX && jumpyX <= stageX + stageWidth+character.w) {
-                character.y=stage.y-character.h;
-                return true;
-            } // function below still has to be checked
-            if (jumpyX+character.w >= stageX && jumpyX <= stageX + stageWidth+character.w) {
-                
-                return true;
-            }
+        // check if stage and character are at the same height
+        if (jumpyY + character.upSpeed > stageY && jumpyY + character.upSpeed < stageY + 22) {
 
+            // check left corner of character with right corner of stage
+            if (jumpyX <= stageX + (stage.width - 15) && (jumpyX + character.w) > (stageX + 15)) {
+                character.y = stage.y - character.h;
+                return true;
+            } // check right vorner of character with left corner of stage
 
         };
 
@@ -167,20 +188,31 @@ let intervalId;
 function run() {
     intervalId = setInterval(() => {
 
-        ctx.clearRect(0, 0, 400, 600)
-        character.draw();
+        ctx.clearRect(0, 0, 400, 600);
+        canvasObj.move();
+        canvasObj.draw();
         newStage();
         updateStages();
+
+
+        character.draw();
         if (character.jumpButton) {
             character.jump()
         };
         if (character.upSpeed < 0) {
             character.land();
         };
-        if(character.downMovement){
+        if (character.downMovement) {
             character.moveDown();
+            character.fallOff = true;
         }
-        
+        if (character.fallOff) {
+            character.fallOffTheEdge();
+        }
+        if (character.y > canvas.height + character.h) {
+            stop();
+        }
+
         counter += 1
 
     }, 1000 / 60);
@@ -209,5 +241,7 @@ document.addEventListener('keydown', event => {
 startbtn.addEventListener('click', run)
 stopbtn.addEventListener('click', stop)
 
+window.onload=canvasObj.draw();
+window.onload=character.draw();
 
 /* */
