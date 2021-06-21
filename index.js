087d486e84cc43d455671bce28190ba0b729cde9
Hannes Polte <hannes.polte@gmx.de>
2021-06-21 11:35:26 +0200

    logic half way done -  landing on stages doenst work perfectly

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 
 const jumpy = document.getElementById('character')
+const startbtn = document.getElementById('startbtn');
+const stopbtn = document.getElementById('stopbtn');
 
 // the canvas
 const canvas = document.getElementById('canvas');
@@ -20,19 +22,19 @@ let character = {
     jumpCounter: 0,
     acceleration: 0.1,
     upSpeed: 12,
-    downSpeed: 5,
+    canvasSpeed: 2.5,
     jumpButton: false,
+    downMovement:false,
     draw: function () {
         ctx.drawImage(this.img, this.x, this.y, this.w, this.h)
     },
     jump: function () {
+        this
         this.y -= this.upSpeed;
         this.jumpCounter += 1;
         this.upSpeed -= this.acceleration;
-        if (this.jumpCounter % 10 === 0) { this.acceleration += 0.05 }
-        //if(this.jumpCounter>=)
+        if (this.jumpCounter % 10 === 0) { this.acceleration += 0.05 };
         if (this.y + this.upSpeed >= canvas.height - 40) {
-            console.log('down')
             this.y = canvas.height - 40
             this.jumpButton = false;
             this.jumpCounter = 0;
@@ -41,11 +43,25 @@ let character = {
         }
 
     },
+    land: function () {
+        if (checkStages()) {
+            this.jumpButton = false;
+            this.jumpCounter = 0;
+            this.acceleration = 0.1;
+            this.downMovement=true;
+            this.upSpeed=12;
+        }
+
+    },
     moveLeft: function () {
         this.x -= 12
     },
     moveRight: function () {
         this.x += 12
+    },
+    moveDown:function(){
+        this.y+=this.canvasSpeed;
+
     }
 
 
@@ -72,26 +88,26 @@ class Stage {
         this.x = x;
         this.y = y;
         this.color = color;
-        this.speed = 1.5;
+        this.speed = 2.5;
     }
 };
 
 function newStage() {
 
-    if (counter % 190 === 0) {
+    if (counter % 80 === 0) {
         counter = 0;
-        let minGap = 55;
-        let maxGap = 150;
-        let x = 0;
-        let y = 0;
-        let width = Math.floor(Math.random() * canvas.width - minGap);
+        let minWidth = 55;
+        let maxWidth = 150;
+        let x = Math.floor(Math.random() * (canvas.width - minWidth));
+        let y = -15;
+        let width = Math.floor(Math.random() * (maxWidth - minWidth)) + minWidth;
+        let color = '#3D290F'
+        //let width = Math.floor(Math.random() * canvas.width - minGap);
         let height = 15;
-        let minWidth = canvas.width - width;
-        let secondX = width + Math.floor(Math.random() * ((maxGap - minGap))) + minGap;
-        let color = '#3D290F';
+
 
         stageList.push(new Stage(width, height, x, y, color))
-        stageList.push(new Stage(minWidth, height, secondX, y, color))
+
     }
 }
 
@@ -105,18 +121,52 @@ function updateStages() {
             ctx.fillRect(e.x, e.y, e.width, e.height);
 
             if (e.y > canvas.height) {
-                stageList.splice(0, 2);
+                stageList.splice(0, 1);
 
             }
 
         })
     }
 }
-// run function
 
+
+// check for stages to land on 
+
+function checkStages() {
+    let jumpyX = character.x;
+    let jumpyY = character.y + character.h;
+    for (let i = 0; i < stageList.length; i++) {
+        let stage = stageList[i];
+        let stageX = stage.x;
+        let stageY = stage.y;
+        let stageWidth = stage.width;
+        if (jumpyY + character.upSpeed > stageY && jumpyY + character.upSpeed < stageY + 20) {
+            if (jumpyX >= stageX && jumpyX <= stageX + stageWidth+character.w) {
+                character.y=stage.y-character.h;
+                return true;
+            } // function below still has to be checked
+            if (jumpyX+character.w >= stageX && jumpyX <= stageX + stageWidth+character.w) {
+                
+                return true;
+            }
+
+
+        };
+
+    }
+    return false
+
+}
+
+
+
+
+// run function
+let intervalId;
 
 function run() {
-    const intervalId = setInterval(() => {
+    intervalId = setInterval(() => {
+
         ctx.clearRect(0, 0, 400, 600)
         character.draw();
         newStage();
@@ -124,6 +174,13 @@ function run() {
         if (character.jumpButton) {
             character.jump()
         };
+        if (character.upSpeed < 0) {
+            character.land();
+        };
+        if(character.downMovement){
+            character.moveDown();
+        }
+        
         counter += 1
 
     }, 1000 / 60);
@@ -131,9 +188,13 @@ function run() {
 
 }
 
+function stop() {
+    clearInterval(intervalId);
+}
 
 
-run();
+
+
 
 document.addEventListener('keydown', event => {
 
@@ -144,6 +205,9 @@ document.addEventListener('keydown', event => {
     if (event.key === 'ArrowRight') { character.moveRight() }
 }
 )
+
+startbtn.addEventListener('click', run)
+stopbtn.addEventListener('click', stop)
 
 
 /* */
